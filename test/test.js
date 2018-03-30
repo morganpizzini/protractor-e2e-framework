593da8efca74f8c9f35524ff81b2652598a28207
Morgan Pizzini <morgan.pizzini@hotmail.com>
2018-03-30 09:24:32 +0200

    added test

---
+++ test/test.js
@@ -0,0 +1,9 @@
+'use strict';
+var expect = require('chai').expect;
+var index = require('../dist/index.js');
+
+describe('dummy test for publishing', () => {
+    it('should..', () => {
+        expect(true).to.equal(true);
+    });
+});
