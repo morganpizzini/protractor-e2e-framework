808f7edb3000e27ab39ee20f311975a68bf0d716
Morgan Pizzini <morgan.pizzini@hotmail.com>
2018-04-03 08:58:12 +0200

    fix export

--- index.ts
+++ index.ts
@@ -2,10 +2,14 @@ import {BasePage} from './lib/entities/page.po';
 import { FrameworkConfiguration } from './lib/config/frameworkConfiguration';
 import { ProtractorUtils } from './lib/utils/protractorUtils';
 import { UrlSchema } from './lib/models/urlSchema';
+import { INavigationFlow } from './lib/workflows/INavigationFlow';
+import { NavigationBaseFlow } from './lib/workflows/NavigationBaseFlow';
 
 export {
     BasePage,
     FrameworkConfiguration,
     ProtractorUtils,
-    UrlSchema
+    UrlSchema,
+    INavigationFlow,
+    NavigationBaseFlow
 };
